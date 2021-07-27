7c4fb24ee12e2a256b32c42a5894f46c7567a696
nanairoaisu <b1018078@fun.ac.jp>
2021-07-27 16:20:18 +0900

    feat/fragmentの追加とファイル構成

---
+++ app/src/main/java/com/example/contributorsapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.contributorsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.contributorsapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
