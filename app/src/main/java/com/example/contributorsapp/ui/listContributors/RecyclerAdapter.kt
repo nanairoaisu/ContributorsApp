b56492cd94a613af0b467ae855fad6e3aabd3c25
nanairoaisu <b1018078@fun.ac.jp>
2021-07-31 17:15:20 +0900

    fix:imageの追加と文字サイズの変更など

--- app/src/main/java/com/example/contributorsapp/ui/listContributors/RecyclerAdapter.kt
+++ app/src/main/java/com/example/contributorsapp/ui/listContributors/RecyclerAdapter.kt
@@ -1,15 +1,18 @@
 package com.example.contributorsapp.ui.listContributors
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.example.contributorsapp.R
 import com.example.contributorsapp.databinding.ListContributorBinding
 import com.example.contributorsapp.model.ContributorsData
 
-class RecyclerAdapter(private var data: List<ContributorsData>): RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder>() {
+class RecyclerAdapter(val context: Context, private var data: List<ContributorsData>): RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder>() {
 
     class RecyclerViewHolder(val binding: ListContributorBinding): RecyclerView.ViewHolder(binding.root)
 
@@ -34,6 +37,12 @@ class RecyclerAdapter(private var data: List<ContributorsData>): RecyclerView.Ad
     override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
         val contributor = data[position]
         holder.binding.viewModel = ContributorsData(contributor.id, contributor.login, contributor.contributions, contributor.avatar_url)
+
+        Glide.with(context)
+            .load(contributor.avatar_url)
+            .circleCrop()
+            .transition(DrawableTransitionOptions.withCrossFade()) // default is 300
+            .into(holder.binding.ivIcon)
 
         holder.binding.layoutList.setOnClickListener {
             listener.onItemClickListener(it, position, contributor)
